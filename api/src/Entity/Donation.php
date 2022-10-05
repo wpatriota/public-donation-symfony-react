8003c48a48bc20a36abdc4054e13dbf8a0e3865c
Wellington Patriota <w_patriota@hotmail.com>
2022-10-05 14:03:56 -0300

    first commit

---
+++ api/src/Entity/Donation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\DonationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(mercure: true)]
+#[ORM\Entity(repositoryClass: DonationRepository::class)]
+class Donation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $donation_value = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $payment = null;
+
+    #[ORM\ManyToOne(inversedBy:'donors')]
+    #[Assert\NotNull]
+    public ?Donor $donor = null;
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDonationValue(): ?float
+    {
+        return $this->donation_value;
+    }
+
+    public function setDonationValue(float $donation_value): self
+    {
+        $this->donation_value = $donation_value;
+
+        return $this;
+    }
+
+    public function getPayment(): ?string
+    {
+        return $this->payment;
+    }
+
+    public function setPayment(string $payment): self
+    {
+        $this->payment = $payment;
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Donor.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\DonorRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(mercure: true)]
+#[ORM\Entity(repositoryClass: DonorRepository::class)]
+class Donor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?float $donation_value = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDonationValue(): ?float
+    {
+        return $this->donation_value;
+    }
+
+    public function setDonationValue(float $donation_value): self
+    {
+        $this->donation_value = $donation_value;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
